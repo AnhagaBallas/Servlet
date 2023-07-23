4e50f514629f67922656f331fa0df1141ca925a3
AnhagaBallas <mickle1212@mail.ru>
2023-07-23 15:53:39 +0300

    first commit

---
+++ servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,34 @@
+package ru.netology.repository;
+
+import ru.netology.model.Post;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+
+// Stub
+public class PostRepository {
+    private AtomicLong id = new AtomicLong();
+    private Map<Long, Post> postList = new HashMap<>();
+
+    public List<Post> all() {
+        return new ArrayList<>(postList.values());
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(postList.get(id));
+    }
+
+    public Post save(Post post) {
+        id.incrementAndGet();
+        postList.put(id.get(), post);
+        return post;
+    }
+
+    public void removeById(long id) {
+        try {
+            postList.remove(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
