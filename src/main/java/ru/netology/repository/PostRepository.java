95be22b06c0398c069244dda0bbc012b739abd2b
AnhagaBallas <102907803+AnhagaBallas@users.noreply.github.com>
2023-07-28 15:44:02 +0300

    Add files via upload

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,35 @@
+package ru.netology.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.netology.model.Post;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class PostRepository {
+    private AtomicLong id = new AtomicLong();
+    private Map<Long, Post> postList = new ConcurrentHashMap<>();
+
+    public List<Post> all() {
+        return new ArrayList<>(postList.values());
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(postList.get(id));
+    }
+
+    public Post save(Post post) {
+        id.incrementAndGet();
+        postList.put(id.get(), post);
+        return post;
+    }
+
+    public void removeById(long id) {
+        try {
+            postList.remove(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -0,0 +1,57 @@
+package ru.netology.servlet;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.JavaConfig;
+import ru.netology.controller.PostController;
+import ru.netology.repository.PostRepository;
+import ru.netology.service.PostService;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet {
+    private PostController controller;
+    private String POST = "POST";
+    private String DELETE = "DELETE";
+    private String GET = "GET";
+    private String API = "/api/posts";
+
+
+    @Override
+    public void init() {
+        final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
+        controller = context.getBean(PostController.class);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        // если деплоились в root context, то достаточно этого
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+            // primitive routing
+            if (method.equals(GET) && path.equals(API)) {
+                controller.all(resp);
+                return;
+            } else if (method.equals(GET) && path.matches(API + "\\d")) {
+                // easy way
+                final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+                controller.getById(id, resp);
+                return;
+            } else if (method.equals(POST) && path.equals(API)) {
+                controller.save(req.getReader(), resp);
+                return;
+            } else if (method.equals(DELETE) && path.matches(API + "\\d")) {
+                // easy way
+                final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+                controller.removeById(id, resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
